253ca3ef97b86c6a7b6b47999c0df0bc97abb119
anshuman <goyalanshuman4@gmail.com>
2020-04-09 17:53:29 +0530

    adding user interface

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,3 +1,4 @@
+"""Defines URL patterns for learning_logs"""
 from django.conf.urls import url
 
 from . import views
@@ -5,4 +6,8 @@ from . import views
 urlpatterns = [
     #home page
     url(r'^$', views.index, name='index'),
+    #show all topics
+    url(r'^topics/$', views.topics, name = 'topics'),
+    #page for adding a new topic
+    url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import TopicForm
+
 
 def index(request):
     """the home page for learning"""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """show all topics"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+
+def new_topic(request):
+    """add new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
