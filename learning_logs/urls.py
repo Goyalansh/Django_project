0b5c8116a0c84bac33448506bbea06216571baa2
anshuman <goyalanshuman4@gmail.com>
2020-04-10 17:13:39 +0530

    made topic functionality stable

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #home page
     url(r'^$', views.index, name='index'),
     #show all topics
-    url(r'^topics/$', views.topics, name = 'topics'),
+    url(r'^topics/$', views.topics, name='topics'),
     #page for adding a new topic
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def topics(request):
     """show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.order_by('data_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -24,3 +24,12 @@ def new_topic(request):
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = TopicForm()
+    else:
+        #Post data submitted; process data.
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
