9bb85e2af0fb53bb135365dddf2d0d2bf4c40b56
anshuman <goyalanshuman4@gmail.com>
2020-04-11 01:40:41 +0530

    made links work properly

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),
     #page for adding a new topic
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,6 +6,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .forms import TopicForm
 
+from .models import Topic
+from .forms import TopicForm, EntryForm
+
+
 
 def index(request):
     """the home page for learning"""
@@ -33,3 +37,11 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
