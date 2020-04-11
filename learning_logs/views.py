e1e011db9ddde65b654cc953e4ae63e737d808b2
anshuman <goyalanshuman4@gmail.com>
2020-04-11 21:49:21 +0530

    added entries field

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -45,3 +45,27 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'post':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # Post data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit = False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+        
+        
+    context = {'topic': topic, 'form': form }
+
+    return render(request, 'learning_logs/new_entry.html', context)
+            
+
+    
