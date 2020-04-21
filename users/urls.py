d42db8a03d4b7b074a214f897cffdf63e0ca6c8e
anshuman <goyalanshuman4@gmail.com>
2020-04-21 20:07:44 +0530

    added logout and registration functionality

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 """define url patterns for users"""
 
+# LoginView takes care of username and password
+
 from django.contrib.auth.views import LoginView
 
 from django.urls import path
 from . import views
 app_name = "users"
 urlpatterns = [
+  # login page
   path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+  # logout page
+  path('logout/', views.logout_view, name='logout'),
+  path('register/', views.register, name='register'),
 ]
